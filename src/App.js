df60d322531c8f507ef5337c5f40698ce047fde7
JorgeTheCreator <JrgPeguero@yahoo.com>
2018-08-20 00:52:42 -0400

    add slider and images

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from "./components/header_footer/Header.js"
+import Featured from "./components/featured"
+
 
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height:"1500px",background:"blue"}}>
+      <div className="App" style={{height:"1500px",background:"purple"}}>
         <Header/>
+        <Featured/> 
       </div>
     );
   }
